4bb424297d552e925ce84e540e64048b398cd735
jcelder <jcelder88@gmail.com>
2018-09-24 20:25:15 -0700

    Replace placeholder with gif

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,7 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) {
-  Dancer.call(this, top, left, timeBetweenSteps);
+  var character = 'hula';
+  Dancer.call(this, top, left, character, timeBetweenSteps);
+  this.$node.addClass('dancer__blinky');
 };
 
 BlinkyDancer.prototype = Object.create(Dancer.prototype);
